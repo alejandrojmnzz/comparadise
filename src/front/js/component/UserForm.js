354b3061bddf0416fd412062a78e1960dd5deff5
UkuJoeMP <josemipinton@gmail.com>
2025-01-06 23:54:42 +0000

    game form

--- src/front/js/component/UserForm.js
+++ src/front/js/component/UserForm.js
@@ -23,91 +23,84 @@ import React, { useState } from 'react'
     // }
 ;
 
-// Datos formulario
-
+// Form data
 return (
-    <div>
-        <h1>Formulario Añadir Videojuego</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+  <form onSubmit={handleSubmit} encType="multipart/form-data">
+      {/* First Section */}
+      <div>
+        <label>Game Name:</label>
+        <input 
+        type="text"
+        name="gameName" 
+        value={formData.gameName} 
+        onChange={handleChange} 
+        required />
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+      <div>
+        <label>Image:</label>
+        <input type="file" name="image" accept="image/*" onChange={handleChange} required />
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+      {/* Second Section */}
+      <div>
+        <label>Genre:</label>
+        <select name="genre" value={formData.genre} onChange={handleChange} required>
+          <option value="">Select a genre</option>
+          <option value="Action (shooter)">Action (shooter)</option>
+          <option value="Action-adventure">Action-adventure</option>
+          <option value="Adventure">Adventure</option>
+          <option value="Casual">Casual</option>
+          <option value="Puzzle">Puzzle</option>
+          <option value="Role-playing">Role-playing</option>
+          <option value="Simulation (sports, racing)">Simulation (sports, racing)</option>
+          <option value="Strategy">Strategy</option>
+        </select>
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+      <div>
+        <label>Game Modes:</label>
+        <select name="modes" value={formData.modes} onChange={handleChange} required>
+          <option value="">Select a mode</option>
+          <option value="Campaign">Campaign</option>
+          <option value="Capture the flag">Capture the flag</option>
+          <option value="Deathmatch">Deathmatch</option>
+          <option value="Oleada">Oleada</option>
+          <option value="Multiplayer">Multiplayer</option>
+        </select>
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+      <div>
+        <label>Release Date:</label>
+        <input type="date" name="releaseDate" value={formData.releaseDate} onChange={handleChange} required />
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
+      {/* Third Section */}
+      <div>
+        <label>System Requirements:</label>
+        <textarea name="systemRequirements" value={formData.systemRequirements} onChange={handleChange} required />
+      </div>
 
-            <div>
-                <label>Nombre del juego:</label>
-                <input
-                type = "text"
-                name = "name"
-                value = {FormData.name}
-                onChange={handleChange}
-                />
-            </div>
-        </form>
-    </div>
+      <div>
+        <label>Achievements:</label>
+        <input type="text" name="achievements" value={formData.achievements} onChange={handleChange} />
+      </div>
+
+      <div>
+        <label>Media (Images or Videos):</label>
+        <input type="file" name="media" accept="image/*,video/*" onChange={handleChange} />
+      </div>
+
+      <div>
+        <label>Rating:</label>
+        <select name="rating" value={formData.rating} onChange={handleChange} required>
+          <option value="">Select a rating</option>
+          <option value="G">G</option>
+          <option value="PG">PG</option>
+          <option value="PG-13">PG-13</option>
+          <option value="R">R</option>
+        </select>
+      </div>
+      </form>
 )
